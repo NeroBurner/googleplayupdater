c368d3349b5cc8abe873574c5c1345f01cb29391
NeroBurner <neroburner@hotmail.de>
2015-09-02 20:31:46 +0200

    update config_example

--- config_example.py
+++ config_example.py
@@ -1,3 +1,6 @@
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+from __future__ import unicode_literals
 
 LANG            = "en_US" # can be en_US, fr_FR, ...
 ANDROID_ID      = None # "38c6523ac43ef9e1"
@@ -6,6 +9,7 @@ GOOGLE_PASSWORD = None # 'yourpassword'
 AUTH_TOKEN      = None # "yyyyyyyyy"
 
 # force the user to edit this file
-if any([each == None for each in [ANDROID_ID, GOOGLE_LOGIN, GOOGLE_PASSWORD]]):
+if ANDROID_ID == NONE
+    or all([each == None for each in [GOOGLE_LOGIN, GOOGLE_PASSWORD, AUTH_TOKEN]]):
     raise Exception("config.py not updated")
 
